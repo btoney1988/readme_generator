924ee3f7a897910d7ea92b993914956075305f13
Brian Toney <btoney1988@gmail.com>
2020-10-13 11:06:23 -0400

    initial commit with written out readme and questions

---
+++ index.js
@@ -0,0 +1,67 @@
+const inquirer = require("inquirer");
+const generatorMarkdown = require("./generateMarkdown")
+const fs = require("fs");
+
+// array of questions for user
+const questions = [
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "UserName",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "Email",
+  },
+
+  {
+    type: "input",
+    message: "What is the title for your project?",
+    name: "Title",
+  },
+  {
+    type: "input",
+    message: "Please give description of your project.",
+    name: "Description",
+  },
+  {
+    type: "input",
+    message: "What necessary dependencies must be installed to run this app?",
+    name: "Installation",
+  },
+  {
+    type: "input",
+    message: "What is this app used for?",
+    name: "Usage",
+  },
+  {
+    type: "input",
+    message: "What license was used for this README?",
+    name: "License",
+  },
+
+  {
+    type: "input",
+    message: "Please add contributors",
+    name: "Contributor",
+  },
+  {
+    type: "input",
+    message: "What command do you use to test this App?",
+    name: "Test",
+  }
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
