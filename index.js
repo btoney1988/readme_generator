b1c4968fa7c65ade6671cb92607890906cbc4bfb
Brian Toney <btoney1988@gmail.com>
2020-10-13 11:11:12 -0400

    changed readme setup to generatemarkdown

--- index.js
+++ index.js
@@ -7,49 +7,49 @@ const questions = [
   {
     type: "input",
     message: "What is your GitHub username?",
-    name: "UserName",
+    name: "userName",
   },
   {
     type: "input",
     message: "What is your email address?",
-    name: "Email",
+    name: "email",
   },
 
   {
     type: "input",
     message: "What is the title for your project?",
-    name: "Title",
+    name: "title",
   },
   {
     type: "input",
     message: "Please give description of your project.",
-    name: "Description",
+    name: "description",
   },
   {
     type: "input",
     message: "What necessary dependencies must be installed to run this app?",
-    name: "Installation",
+    name: "installation",
   },
   {
     type: "input",
     message: "What is this app used for?",
-    name: "Usage",
+    name: "usage",
   },
   {
     type: "input",
     message: "What license was used for this README?",
-    name: "License",
+    name: "license",
   },
 
   {
     type: "input",
     message: "Please add contributors",
-    name: "Contributor",
+    name: "contributor",
   },
   {
     type: "input",
     message: "What command do you use to test this App?",
-    name: "Test",
+    name: "test",
   }
 
 ];
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,33 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
 
-`;
-}
+## Description
+ - ${discritption}
+
+## Table of contents
+ - ${tableOfContents}
+
+## Installation
+ - ${installation}
+
+## Usage
+ - ${usage}
+
+## License
+ - ${license}
+
+## Contributing
+ - ${contributing}
+
+## Tests
+ - ${tests}
+
+## Questions
+ - "https://www.github.com/${github}"
+ - "Feel frere to email me at ${email}"
+`
+};
 
 module.exports = generateMarkdown;
